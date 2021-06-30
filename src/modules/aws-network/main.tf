6367d76634ed251a30df369f9d9cd78968dcc891
Silvio Silva <smsilva@gmail.com>
2021-06-29 22:44:07 -0300

    wip

--- src/modules/aws-network/main.tf
+++ src/modules/aws-network/main.tf
@@ -18,22 +18,20 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name = "${var.prefix}-vpc"
-  cidr = var.cidr
-
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
-  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
-
+  name               = "${var.prefix}-vpc"
+  cidr               = var.cidr
+  azs                = data.aws_availability_zones.available.names
+  private_subnets    = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets     = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
   enable_nat_gateway = true
   enable_vpn_gateway = var.vpn_gateway
-
-  tags = var.tags
+  tags               = var.tags
 }
 
 module "app_security_group" {
-  source              = "terraform-aws-modules/security-group/aws//modules/web"
-  version             = "4.3.0"
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.3.0"
+
   name                = "${var.prefix}-sg-web"
   description         = "Security group for web-servers with HTTP ports open within VPC"
   vpc_id              = module.vpc.vpc_id
@@ -42,8 +40,9 @@ module "app_security_group" {
 }
 
 module "lb_security_group" {
-  source              = "terraform-aws-modules/security-group/aws//modules/web"
-  version             = "4.3.0"
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "4.3.0"
+
   name                = "${var.prefix}-sg-lb"
   description         = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id              = module.vpc.vpc_id
